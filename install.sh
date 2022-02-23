71b4527920f33c6be5a40978d1901b6269880ad6
Pedro Portales <pedroportales>
2022-02-23 09:59:10 -0300

    Local Installation

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
-if [[ $(id -u) -ne 0 ]] ; then printf "Please run as root\n" ; exit 1 ; fi
+if [[ $(id -u) -ne 0 ]] ; then printf 'Please create ~/.local/bin if not exist, and add it to $PATH\n' ; exit 1 ; fi
 
-cd /usr/bin
+cd ~/.local/bin/
 curl -o ppfetch https://raw.githubusercontent.com/pedroportales/ppfetch/main/ppfetch
 printf "\nppfetch has a successful installation, please consider run it now. :D\n"
