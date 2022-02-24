f3e9f6f6f868161a0915b679ac3efe47f22743fc
Pedro Portales <pedroportales@yandex.com>
2022-02-23 22:06:32 -0300

    Installation Script add Run Permission

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ set -e
 if [[ $(id -u) -ne 0 ]] ; then printf 'Please create ~/.local/bin if not exist, and add it to $PATH\n' ; exit 1 ; fi
 
 cd ~/.local/bin/
-curl -o ppfetch https://raw.githubusercontent.com/pedroportales/ppfetch/main/ppfetch
+curl -o ppfetch https://raw.githubusercontent.com/pedroportales/ppfetch/main/ppfetch && chmod +x ppfetch
 printf "\nppfetch has a successful installation, please consider run it now. :D\n"
