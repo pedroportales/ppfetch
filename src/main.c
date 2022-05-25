4d60bf6e12e65f4c5235928d7e1e416187caf9f9
Pedro Portales <pedroportales@riseup.net>
2022-05-25 09:20:22 -0300

    add user and hostname

--- src/main.c
+++ src/main.c
@@ -1,8 +1,16 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <limits.h>
 
-int main() {
-  
+int main(void) {
+  char hostname[HOST_NAME_MAX + 1];
+  char *user = getlogin();
+  gethostname(hostname, HOST_NAME_MAX + 1);
+
+  printf("%s@%s\n", user, hostname);
+
+  return EXIT_SUCCESS;
 }
 
