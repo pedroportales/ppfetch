7ee3933232d2fbcb5f809acc70197beaf59f7bc8
Pedro Portales <pedroportales@riseup.net>
2022-05-26 09:54:45 -0300

    add shell and change user to system env

--- src/main.c
+++ src/main.c
@@ -1,19 +1,27 @@
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/utsname.h>
 
 int main(void) {
-  struct utsname unameData;
+  struct utsname unameData; // machine and kernel data
   uname(&unameData);
-  char *user = getlogin();
-  //  char kernel_name = uname(&os);
+  //  char *user = getlogin(); // username
+  char *shell = getenv("SHELL");
+  char *user = getenv("USER");
+
+  // distro name
+  FILE *fp;
+  char distroname[20] = "";
+  fp = popen("lsb_release -ds", "r");
+  fgets(distroname, 20, fp);
+  pclose(fp);
+    
   printf("%s@%s\n", user, unameData.nodename);
-  printf("system name = %s\n", unameData.sysname);
-  printf("release = %s\n", unameData.release);
-  printf("version = %s\n", unameData.version);
-  printf("machine = %s\n", unameData.machine);
-  
+  printf("system ~ %s\n", distroname);
+  printf("kernel ~ %s %s\n", unameData.sysname, unameData.release);
+  printf("arch ~ %s\n", unameData.machine);
+  printf("shell ~ %s\n", shell);
+    
   return 0;
 }
 
